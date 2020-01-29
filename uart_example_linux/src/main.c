36df955141f3194f943b7670607c6e830dd9c243
Demon000 <demonsingur@gmail.com>
2020-01-29 14:24:55 +0200

    uart_example_linux: use noncanonical mode and avoid useless prints

--- uart_example_linux/src/main.c
+++ uart_example_linux/src/main.c
@@ -12,7 +12,7 @@
 #include <string.h>
 #include "uart.h"
 
-#define LOOPBACK_FORMAT "loopback(%d): %s"
+#define LOOPBACK_FORMAT "loopback: %s\r\n"
 #define LOOPBACK_FORMAT_LEN strlen(LOOPBACK_FORMAT)
 #define MAX_READ_SIZE 235
 #define MAX_LOOPBACK_SIZE MAX_READ_SIZE + LOOPBACK_FORMAT_LEN
@@ -24,7 +24,7 @@ int main() {
 	dev.filename = "/dev/ttyUL1";
 	dev.rate = B9600;
 
-	rc = uart_start(&dev, true);
+	rc = uart_start(&dev, false);
 	if (rc) {
 		return rc;
 	}
@@ -32,18 +32,17 @@ int main() {
 	char read_data[MAX_READ_SIZE];
 	char loopback_data[MAX_LOOPBACK_SIZE];
 	size_t read_data_len;
-	size_t loopback_data_len;
 
-	loopback_data_len = uart_writes(&dev, "UART DEMO\r\n");
-	printf("loopback_data_len: %d\n", loopback_data_len);
+	printf("UART DEMO\r\n");
+	uart_writes(&dev, "UART DEMO\r\n");
 
 	while (1) {
 		read_data_len = uart_reads(&dev, read_data, MAX_READ_SIZE);
 
 		if (read_data_len > 0) {
-			printf("read(%d): %s", read_data_len, read_data);
-			snprintf(loopback_data, MAX_LOOPBACK_SIZE, LOOPBACK_FORMAT, read_data_len, read_data);
-			loopback_data_len = uart_writes(&dev, loopback_data);
+			printf("%s", read_data);
+			snprintf(loopback_data, MAX_LOOPBACK_SIZE, LOOPBACK_FORMAT, read_data);
+			uart_writes(&dev, loopback_data);
 		}
 	}
 
